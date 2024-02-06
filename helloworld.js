18fd3c7f0088774af8569c604f7b5319fb0bd123
Craigen Chitate <craigenchitate7@gmail.com>
2024-02-06 04:49:53 +0200

    added helloworld.js file

---
+++ helloworld.js
@@ -0,0 +1,11 @@
+
+function Hello() {
+    const greeting = "Hello";
+    const name = "Craigen";
+    const message = `${greeting} ${name}`;
+    console.log(message);
+    return message;
+}
+
+// Calling the function
+Hello();
